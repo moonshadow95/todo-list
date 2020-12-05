b7085e5b802b8d36b0c0aa84699bab555ce5ce58
moonshadow <last_enemy@naver.com>
2020-12-05 14:32:23 +0900

    toDoList201205

---
+++ index.js
@@ -0,0 +1,13 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
